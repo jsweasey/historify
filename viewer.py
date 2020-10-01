0f54f72ab2caef0d8dff18f49f65c122dc270b00
jsweasey <jack@sweasey.xyz>
2020-10-01 14:35:14 +0100

    error checking for when no END defined for song

--- viewer.py
+++ viewer.py
@@ -17,9 +17,12 @@ for entry in clean_data:
         song_data_dict.update({entry[2]:[]})
     if entry[0] == 'START':
 
-    if entry[0] == 'END':
-        total_time_listened += (float(entry[3]) - float(entry[4]))
-        total_time_paused += (float(entry[4]))
+        if (i > 0) and clean_data[i-1][0] != 'END': #Removes entries which have no defined END
+            if song_data_dict[clean_data[i-1][2]]['repeats'] < 2:
+                song_data_dict.pop(clean_data[i-1][2])
+            else:
+                song_data_dict[clean_data[i-1][2]]['repeats'] -= 1
 
+        song_data_dict[entry[2]]['repeats'] += 1
 print(total_time_listened)
 print(total_time_paused)
